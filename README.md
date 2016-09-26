b414f6dda7a2dd0eee01ffd84539c12f60c3cc25
Kristian Videmark Parkov <parkov@bitkompagniet.dk>
2016-09-26 21:47:37 +0200

    Refactor to use any function and add a cooldown

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-A very simple promise queue. On creation, takes a function that should return a promise. 
-You can then push values that are passed to the function as the queue is processed. Every time you push an
-item, a promise is returned that will be resolved with the value when the item has been processed.
+Push functions returning a function to the queue. Set the concurrency, and
+optionally a cooldown.
 
 ## Install
 
@@ -11,29 +10,35 @@ npm install housecall
 ## Usage
 
 ```javascript
-var housecall = require("housecall");
-var Promise = require("bluebird");
-var request = Promise.promisify(require('request'));
+var housecall = require('housecall');
+var axios = require('axios');
 
-// housecall(function(value) { return promise; }, [maximum concurrent queue items])
-var queue = housecall(request, 1);
+var queue = housecall({ concurrency: 2, cooldown: 1000 });
 
-queue.push("https://twitter.com/").spread(function(response, body) {
+queue.push(() => axios.get('https://twitter.com/').then((response, body) => {
 	// The eventual response of Twitter
-});
+}));
+
+queue.push(() => axios.get('https://some.com/').then((response, body) => {
+	// ...
+}));
+
+queue.push(() => axios.get('https://test.com/').then((response, body) => {
+	// This will not run until time of termination of one of the above + 1000 ms.
+}));
 
 ```
 
-It is possible to push an array of items. The return value will be an promise resolving all these items.
+Make sure you wrap the function. Otherwise it will run immediately and defeat the purpose of the queue.
 
 ```javascript
-queue.push(["http://www.google.com", "https://twitter.com/", "https://www.reddit.com/"]).then(function(results) {
-	// Results from the above 3 calls.
-});
+// WILL NOT BE CORRECTLY ENQUEUED
+queue.push(axios.get('https://twitter.com/').then((response, body) => {
+	// The queue will not control the start of execution here.
+}));
 ```
 
-## Promises
+## Errors
 
-You should always initialize housecall with a function that returns a promise. Otherwise, results will not be as expected.
-
-If you want to create a queue for a node style function, utilizing a callback function, you should [promisify that function](https://github.com/petkaantonov/bluebird/blob/master/API.md#promisification).
+The queue continues on any error, but the error will be outputted to `housecall:queue:error`. Set `DEBUG=housecall:queue:error`
+to see it.
--- test/queue.js
+++ test/queue.js
@@ -1,239 +1,49 @@
-var Promise = require("bluebird");
-var should = require("chai").should();
+const should = require("chai").should();
+const queue = require('../lib/queue');
+const Chance = require('chance');
 
-var wait = function(time) {
+describe("queue()", function() {
 
-	return new Promise(function (resolve, reject) {
+	this.timeout(2000000);
 
-		setTimeout(function() {
-
-			resolve("Resolved!");
-
-		}, (time || 1000));
-
+	it('should be a function', function() {
+		queue.should.be.a('function');
 	});
 
-};
-
-describe("Queue(function, [concurrency])", function() {
-
-	this.slow(700);
-
-	var queue = require("../lib/queue");
-
-	it("Should only accept arguments of the correct form", function() {
-
-		var a = function() {};
-		var b = 10;
-		var c = "help!";
-
-		queue.should.be.a("function");
-
-		queue.bind(queue).should.throw(Error);
-		queue.bind(queue, a).should.not.throw(Error);
-		queue.bind(queue, a, b).should.not.throw(Error);
-		queue.bind(queue, c).should.throw(Error);
-		queue.bind(queue, a, c).should.throw(Error);
-		queue(a,b).should.be.an("object");
-
+	it('should construct an object', function() {
+		const q = queue();
+		queue().should.be.an('object');
 	});
 
-	it("Should have the necessary properties and methods", function() {
-
-		var q = queue(function() {});
-
-		q.should.have.property("concurrency", 10);
-		q.should.have.property("action").that.is.a("function");
-		q.should.have.property("items").that.is.an("array");
-		q.should.have.property("active").that.is.a("boolean");
-		q.should.have.property("running").that.is.a("number");
-		q.should.have.property("push").that.is.a("function");
-		q.should.have.property("start").that.is.a("function");
-		q.should.have.property("pause").that.is.a("function");
-		q.should.have.property("clear").that.is.a("function");
-
+	it('should have methods push, delay, ', function() {
+		queue().push.should.be.a('function');
 	});
 
-	it("Should emit events 'added', 'progress' and 'done'", function() {
+	it('should continue on error', function() {
+		const q = queue({ concurrency: 1 });
+		q.push(() => q.delay(10))
+		q.push(() => { throw new Error('hest'); });
+		q.push(() => Promise.resolve().then(() => Promise.reject()))
+		q.push(() => q.delay(10));
 
-		var items = [100, 100, 100];
-		var q = queue(wait, 1);
-		q.pause();
-
-		q.should.have.property("on").that.is.a("function");
-
-		var addedPromise = new Promise(function(resolve, reject) {
-			q.on("added", function(value) {
-				value.should.be.an("object").with.property("then");
-				resolve();
-			});
+		return q.delay(100).then(() => {
+			q.pending().should.equal(0);
+			q.running().should.equal(0);
 		});
-
-		var progressPromise = new Promise(function(resolve, reject) {
-
-			var count = 0;
-
-			q.on("progress", function(value) {
-				value.should.be.a("string");
-				value.should.equal("Resolved!");
-
-				count++;
-
-				if (count === items.length) resolve();
-			});
-		})
-
-		var donePromise = new Promise(function(resolve, reject) {
-			q.on("done", function() {
-				resolve();
-			});
-		});
-
-		q.push(items);
-
-		q.start();
-
-		return Promise.all([addedPromise, progressPromise, donePromise]);
-
 	});
 
-	it("should emit 'done' again, if more items are added after end");
+	it('should wait for a cooldown of 40 ms', function() {
+		const q = queue({ concurrency: 3, cooldown: 40 });
+		const f = index => q.delay(40).then(() => index);
 
-	describe(".push(item)", function() {
+		for (let i = 0; i < 9; i++) q.push(() => f(i));
 
-		this.timeout(10000);
+		q.running().should.equal(3);
 
-		it("Should correctly update items, also when fed arrays", function() {
-
-			var q = queue(function() {});
-			q.pause();
-
-			q.items.length.should.equal(0);
-
-			q.push(100);
-			q.items.length.should.equal(1);
-
-			q.push([100, 100]);
-			q.items.length.should.equal(3);
-
-		});
-
-		it("Should work the queue as expected", function(done) {
-
-			var q = queue(wait, 1);
-			q.pause();
-
-			q.push([100, 100, 100]);
-			q.items.length.should.equal(3);
-
-			q.start();
-
-			wait(20)
-			.then(function() {
-				q.items.length.should.equal(2);
-				return wait(100);
-			})
-			.then(function() {
-				q.items.length.should.equal(1);
-				return wait(100);
-			})
-			.done(function() {
-				done();
-			});
-
-		});
-
-		it("Should return a promise that is resolved eventually with the result to the caller", function() {
-
-			var q = queue(wait, 1);
-			q.pause();
-
-			var promise = q.push(100);
-			promise.should.have.property("then").that.is.a("function");
-
-			promise.isPending().should.equal(true);
-
-			return wait(110)
-			.then(function() {
-				promise.isPending().should.equal(true);
-				q.start();
-				return wait(110);
-			})
-			.then(function() {
-				promise.isPending().should.equal(false);
-				promise.isFulfilled().should.equal(true);
-				promise.value().should.equal("Resolved!");
-			});
-
-		});
-
-		it("should return a Promise.all when adding an array", function() {
-
-			var q = queue(wait, 1);
-			q.pause();
-
-			var promise = q.push([100, 100, 100]);
-			
-			promise.should.have.property("then").that.is.a("function");
-
-			q.start();
-
-			return promise.then(function(results) {
-				results.should.be.an("array");
-				results.length.should.equal(3);
-
-				results.forEach(function(item) {
-					item.should.be.a("string");
-				});
-			});
-
-		});
-
-		it("should fail gracefully and continue even if one or more items are rejected");
-
-	});
-
-	describe(".pause() and .start()", function() {
-
-		this.timeout(5000);
-
-		it("Should stop and restart the queue", function() {
-
-			var q = queue(wait, 1);
-			q.pause();
-
-			q.push([20, 20, 20]);
-			q.items.length.should.equal(3);
-
-			return wait(100)
-			.then(function() {
-				q.items.length.should.equal(3);
-				q.start();
-				return wait(100);
-			})
-			.then(function() {
-				q.items.length.should.equal(0);
-			});
-
-		});
-
-	});
-
-	describe(".clear()", function() {
-
-		it("Should empty out the items", function() {
-
-			var q = queue(wait, 1);
-			q.pause();
-
-			q.push([100, 100]);
-			q.items.length.should.equal(2);
-
-			q.clear();
-			q.items.length.should.equal(0);
-
-		});
-
+		return q.delay(60)
+			.then(() => q.running().should.equal(0) && q.delay(40))
+			.then(() => q.running().should.equal(3) && q.delay(40))
+			.then(() => q.running().should.equal(0));
 	});
 	
 });
